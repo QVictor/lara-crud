e55f56abf9204cfc073f9c658576188259936f17
QVictor <QQlogin@yandex.ru>
2021-11-09 16:34:28 +0300

    Создание контроллера и роута

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UsersController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,12 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', 'users');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::resource('users', UsersController::class);
 
 require __DIR__.'/auth.php';
