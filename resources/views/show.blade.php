7d1b9611386ad3ec5fb9b80a11221344b52b682c
QVictor <QQlogin@yandex.ru>
2021-11-14 16:14:20 +0300

    cart for user

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,7 +1,18 @@
 @extends('layout')
 
-@section('title', 'Users')
+@section('title', 'User' . $user->name)
 
 @section('content')
-    {{dd($user)}}
+    <a type="button" class="btn btn-secondary" href="{{route('users.index')}}">Back to users</a>
+    <div class="card mt-3" style="width: 18rem;">
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">id: {{$user->id}}</li>
+            <li class="list-group-item">name: {{$user->name}}</li>
+            <li class="list-group-item">email: {{$user->email}}</li>
+            <li class="list-group-item">created_at: {{$user->created_at->format('d/m/y H:i:s')}}</li>
+            <li class="list-group-item">updated_at: {{$user->updated_at->format('d/m/y H:i:s')}}</li>
+        </ul>
+    </div>
+
+    <td><a type="button" class="btn btn-warning mt-3" href="{{route('users.edit', $user)}}">Edit</a></td>
 @endsection
